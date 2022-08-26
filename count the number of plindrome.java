0bb30e2f43ea0f14e3d83ccfa3448f2befa07ffb
Rohan Sharma <95439130+Rohan24sharma@users.noreply.github.com>
2022-08-26 21:13:36 +0530

    Update count the number of plindrome.java

--- count the number of plindrome.java
+++ count the number of plindrome.java
@@ -100,3 +100,56 @@ class Solution
 // using dynamic programming
 
 
+
+//{ Driver Code Starts
+import java.util.*;
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String str = sc.nextLine();
+			//System.out.println(str.length());
+			Solution ob  = new Solution();
+			System.out.println(ob.countPS(str));
+		t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete below method */
+
+class Solution
+{
+    long ans;
+    long dp[][];
+    long mod = 1000000007;
+    long countPS(String str)
+    {
+        ans = 0;
+        dp = new long[str.length()][str.length()];
+        return dfs(str,0,str.length()-1);
+    }
+    long dfs(String s, int i, int j){
+        if(i==j)return 1;
+        
+        if(i+1 == j)return s.charAt(i)==s.charAt(j)?3:2;
+        if(dp[i][j] != 0)return dp[i][j];
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = (1+dfs(s,i,j-1)+dfs(s,i+1,j))%mod;
+        }
+        else{
+            dp[i][j] = (dfs(s,i+1,j)+dfs(s,i,j-1))%mod; 
+            dp[i][j] = (dp[i][j] - dfs(s,i+1,j-1)+mod)%mod;
+            return dp[i][j];
+        }
+    }
+}
+
+
