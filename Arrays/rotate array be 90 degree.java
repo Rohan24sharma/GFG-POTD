bbe16e8b531227935d620af04119cda4d49aa117
Rohan Sharma <95439130+Rohan24sharma@users.noreply.github.com>
2022-09-03 10:25:58 +0530

    Update rotate array be 90 degree.java

--- Arrays/rotate array be 90 degree.java
+++ Arrays/rotate array be 90 degree.java
@@ -57,3 +57,77 @@ class Solution
         }
     }
 }
+
+
+// without using another matrix
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n) 
+    { 
+        transPose(matrix,n);
+        for(int i = 0;i<n;i++){
+            int l = 0;
+            int h = n-1;
+            while(l<h){
+                int temp = matrix[l][i];
+                matrix[l][i] = matrix[h][i];
+                matrix[h][i] = temp;
+                l++;
+                h--;
+            }
+        }
+    }
+    static void transPose(int arr[][],int n){
+        for(int i = 0;i<n;i++){
+            for(int j = i+1;j<n;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+}
